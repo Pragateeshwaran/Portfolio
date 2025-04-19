b9c6d0bda496e5af90dd5cc03671e2c6c13d3768
Pragateeshwaran <geniuspekka1808@gmail.com>
2025-04-20 04:42:43 +0530

    .

---
+++ src/assets/index.js
@@ -0,0 +1,61 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import Namasivaayam from "./TopTestimonials/Namasivaayam.png";
+import nithish from "./TopTestimonials/nithish.png";
+import nawfal from "./TopTestimonials/nawfal.png";
+import sagar from "./TopTestimonials/sagar.png";
+
+import cuda from "./tech/cuda.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import huggingface from "./tech/huggingface.png";
+import keras from "./tech/keras.png";
+import langchain from "./tech/langchain.png";
+import mongodb from "./tech/mongodb.png";
+import onnx from "./tech/onnx.png";
+import Tensorflow from "./tech/Tensorflow.png";
+import torch from "./tech/torch.png";
+import neo4j from "./tech/Neo4j.png"
+import opencv from "./tech/opencv.png"
+ 
+import mepco from "./education/mepco.png";
+import school from "./education/school.png";
+import xplore from "./education/xplore.png";
+
+ 
+export {
+  Namasivaayam, 
+  sagar,
+  nithish, 
+  nawfal, 
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  cuda,
+  docker,
+  git,
+  huggingface,
+  keras,
+  langchain,
+  mongodb,
+  onnx,
+  Tensorflow,
+  torch,
+  mepco,
+  school,
+  xplore,
+  neo4j,
+  opencv,
+};
