efd36f47c8843e149e0030d73545f54f946f6b30
Pragateeshwaran <geniuspekka1808@gmail.com>
2025-05-08 07:42:48 +0530

    removed unwanted logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             window.scrollTo({ top: 0, behavior: "smooth" });
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Pragateesh &nbsp;
             <span className='sm:block hidden'> | Student</span>
